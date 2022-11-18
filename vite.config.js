11248df621e26864ab9db6bd9ab1785b5eb38ada
alberigi <mateusalberigi@gmail.com>
2022-11-18 17:46:40 -0300

    adicionando e configurando o vitest

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,17 @@
-export default {
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
   root: "src",
   build: {
     outDir: "../dist",
   },
-};
+  test: {
+    globals: true,
+    deps: {
+      inline: [""],
+    },
+    coverage: {
+      exclude: ["src/test"],
+    },
+  },
+});
